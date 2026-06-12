265ab8fa827944de597d6762645b0d5d2f18d27d
timo <271266910@qq.com>
2026-06-13 00:08:51 +0800

    tijiao

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  outputDir: 'docs', // 将 dist 改为 docs
+  publicPath: process.env.NODE_ENV === 'production' ? '/familytree/' : '/' // 替换为你的 GitHub 仓库名
 })
